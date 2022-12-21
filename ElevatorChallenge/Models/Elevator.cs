c788e81e0d5ef2e00be4db4378135479d28e9572
EngBett <engkipkemoi@gmail.com>
2022-12-21 13:48:45 +0300

    add person model

--- ElevatorChallenge/Models/Elevator.cs
+++ ElevatorChallenge/Models/Elevator.cs
@@ -4,15 +4,38 @@ namespace ElevatorChallenge.Models;
 
 public class Elevator
 {
-    public Elevator(string name, int maximumCapacity)
+    public Elevator(string name, int maximumCapacity, int topFloor, int bottomFloor)
     {
         Name = name;
         MaximumCapacity = maximumCapacity;
+        TopFloor = topFloor;
+        BottomFloor = bottomFloor;
     }
-    public Elevator(){}
+
+    public Elevator()
+    {
+    }
+
     public string Name { get; private set; }
     public ElevatorMotionStatuses ElevatorMotionStatus { get; set; } = ElevatorMotionStatuses.Stationary;
     public int CurrentFloor { get; set; }
+    public int TopFloor { get; set; }
+    public int BottomFloor { get; set; }
     public int MaximumCapacity { get; }
     public int CurrentCapacity { get; set; }
+
+    public void Run()
+    {
+
+        if (ElevatorMotionStatus == ElevatorMotionStatuses.MovingDown)
+        {
+            if (CurrentFloor > BottomFloor) CurrentFloor--;
+            else ElevatorMotionStatus = ElevatorMotionStatuses.Stationary;
+        }
+        else
+        {
+            if (CurrentFloor < TopFloor) CurrentFloor++;
+            else ElevatorMotionStatus = ElevatorMotionStatuses.Stationary;
+        }
+    }
 }
---
+++ ElevatorChallenge/Models/Person.cs
@@ -0,0 +1,16 @@
+namespace ElevatorChallenge.Models;
+
+public class Person
+{
+    public Person(int startingFloor, int destinationFloor, Elevator elevator)
+    {
+        StartingFloor = startingFloor;
+        DestinationFloor = destinationFloor;
+        Elevator = elevator;
+    }
+
+    public Person(){}
+    public int StartingFloor { get; set; }
+    public int DestinationFloor { get; set; }
+    public Elevator Elevator { get; set; }
+}
